0f54e807e2ebfd8e1156f114dddf33db92548567
ZJU-Winter <78242467+ZJU-Winter@users.noreply.github.com>
2022-11-11 14:26:09 -0800

    Project2 add test files

--- nachos/test/close1.c
+++ nachos/test/close1.c
@@ -9,11 +9,11 @@ int main() {
     int fd = open(str);
     int res1 = close(fd);
     assert(res1 == 0);
-    if (res1 == 0) { printf("Test Passed: Close Correctly"); }
-    else { printf("Test Failed: Error in Close"); }
+    if (res1 == 0) { printf("Test Passed: Close Correctly\n"); }
+    else { printf("Test Failed: Error in Close\n"); }
     int res2 = close(fd);
     assert(res2 == -1);
-    if (res2 == -1) { printf("Test Passed: Close failed when closing a closed fd"); }
-    else { printf("Test Failed: when closing a closed fd"); }
+    if (res2 == -1) { printf("Test Passed: Close failed when closing a closed fd\n"); }
+    else { printf("Test Failed: when closing a closed fd\n"); }
     return 0;
 }
--- nachos/test/creat1.c
+++ nachos/test/creat1.c
@@ -6,7 +6,7 @@ int main() {
 
 	char* filename="output.txt";
 	int result = creat(filename);
-	if (result != -1) { printf("Test Passed: create 'output.txt' in test directory. "); }
+	if (result != -1) { printf("Test Passed: create 'output.txt' in test directory.\n"); }
 	else { printf("Test Failed."); }
 	return 0;
 }
