31674c399e80f46a08a82d7798726782980d1fc4
Stevie Huang <78242467+ZJU-Winter@users.noreply.github.com>
2022-11-22 17:45:13 -0800

    Proj3 task1

--- nachos/test/sh.c
+++ nachos/test/sh.c
@@ -198,7 +198,7 @@ void runline(char* line) {
 }
 
 int main(int argc, char *argv[]) {
-    char prompt[] = "nachos% ";
+    char *prompt = "nachos% ";
     char *string = "MSRNB";
 
     char buffer[BUFFERSIZE];
