afefc329cd6e55c0d7ba415a0e226eaa53fbd09e
Stevie Huang <78242467+ZJU-Winter@users.noreply.github.com>
2022-11-28 12:39:39 -0800

    Proj3 add testfiles

--- nachos/test/multiswap4.c
+++ nachos/test/multiswap4.c
@@ -1,19 +1,18 @@
 #include "syscall.h"
 #include "stdio.h"
-#define N 8
-int main(int argc, char** argv) {
-    char *progrm = argv[0];
-    printf("Running %d %s.coff processes.", N, progrm);
+#define N 16
+int main() {
+    printf("Running %d swap4.coff processes.");
     int retureValues[N];
     int pids[N];
     for (int i = 0; i < N; i += 1) {
-        pids[i] = exec(progrm, 0, 0);
+        pids[i] = exec("swap4.coff", 0, 0);
     }
     for (int i = 0; i < N; i += 1) {
         join(pids[i], retureValues + i);
     }
     for (int i = 0; i < N; i += 1) {
-        if (retureValues[i] != 0) {
+        if (retureValues[i] != -1000) {
             exit(retureValues[i]);
         }
     }
