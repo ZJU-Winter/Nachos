8d3191baa1e74524fd4a1d3302601fcc1370243e
Stevie Huang <78242467+ZJU-Winter@users.noreply.github.com>
2022-11-29 13:01:28 -0800

    Proj3 part2 add test files

--- nachos/test/multiwrite.c
+++ nachos/test/multiwrite.c
@@ -3,7 +3,7 @@
 #define N 3
 int main() {
     printf("Running %d write10.coff processes.\n", N);
-    exec("mywrite10.coff", 0, 0);
-    exec("mywrite10_1.coff", 0, 0);
-    exec("mywrite10_2.coff", 0, 0);
+    exec("write10.coff", 0, 0);
+    exec("write10_1.coff", 0, 0);
+    exec("write10_2.coff", 0, 0);
 }
